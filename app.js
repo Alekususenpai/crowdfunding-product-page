b4050b4f77dabf3861192ea769900e0e7993f35b
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2022-04-05 00:50:37 +0200

    /

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let icon = document.getElementById('menu-icon');
 let menu = document.getElementById('menu');
+let closeIcon = document.getElementById('close-icon');
 
 function show(){
           menu.classList.toggle('show');
-          
+          closeIcon.classList.toggle('show');
 }
