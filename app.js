7e7529191cc0f161943e8889ab78218028dbc4f6
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2022-04-13 01:30:46 +0200

    modal - selected styles / event

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ let icon = document.getElementById('menu-icon');
 let menu = document.getElementById('menu');
 let closeIcon = document.getElementById('close-icon');
 
-function show(){
+function show() {
           menu.classList.toggle('show');
           closeIcon.classList.toggle('show');
+}
+
+function checker() {
+          let inputs = document.getElementsByTagName('input');
+          for (input of inputs) {
+                    if (input.checked === true) {
+                              console.log('lele')
+                              input.parentElement.style.borderColor = "#3BB3AB";
+                    } else {
+                              input.parentElement.style.borderColor = "rgba(122, 122, 122, 0.075)";
+                    }
+
+          }
 }
