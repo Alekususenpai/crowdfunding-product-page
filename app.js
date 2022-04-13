8b89cec9a3f463c9dfab20d4bb0f58a4fb204501
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2022-04-13 20:58:35 +0200

    modal - selected styles and logic done

--- app.js
+++ app.js
@@ -7,15 +7,21 @@ function show() {
           closeIcon.classList.toggle('show');
 }
 
-function checker() {
-          let inputs = document.getElementsByTagName('input');
-          for (input of inputs) {
-                    if (input.checked === true) {
-                              console.log('lele')
-                              input.parentElement.style.borderColor = "#3BB3AB";
-                    } else {
-                              input.parentElement.style.borderColor = "rgba(122, 122, 122, 0.075)";
-                    }
 
-          }
+let inputs = document.querySelectorAll('input[name="modal"]');
+
+for (let input of inputs) {
+          input.addEventListener('change', () => {
+                    for (let input of inputs) {
+                              if (input.checked === true) {
+                                        input.parentElement.style.borderColor = "#3BB3AB";
+                              } else {
+                                        input.parentElement.style.borderColor = "rgba(122, 122, 122, 0.075)";
+                              }
+
+                    }
+          })
+
+
 }
+
